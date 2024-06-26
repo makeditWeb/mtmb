30db5b869e95d913f8d9a8ff475a901f10cd8d4e
Leemainsw <yebom6476@gmail.com>
2024-06-26 23:28:11 +0900

    feat: 불필요한 로직 제거

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,112 @@ $(document).ready(function () {
     $('#lnb').css('display', isLargeScreen ? 'block' : 'none');
 
     if (isLargeScreen) {
+      //  fulllpage 설정
+      $("#fullpage").fullpage({
+          anchors: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+          menu: "#header",
+          verticalCentered: true, // 세로 중앙 정렬
+          scrollOverflow: true,
+          sectionsColor: ["#000", "#fff"], // 섹션별 컬러
+          navigation: true,
+          keyboardScrolling: true,
+          animateAnchor: true,
+          recordHistory: true,
+          css3: true,
+          navigationPosition: "right",
+          loopHorizontal: false, // 반복여부
+          controlArrows: false, // 슬라이드 좌우 이동 제어
+          passive: false,
+          scrollbars: true,
+          // 모바일에서는 fullpage 초기화
+          responsiveWidth: 992,
+          onLeave: function (anchorLink, index, direction) {
+            var pages = $(".section").length;
+            var currentPage = index - 1;
+          },
+          afterLoad: function (anchorLink, index) {
+            // console.log(index);
+            if (index != "1") {
+              $(".header_i img").fadeIn();
+              $("#top_fixed").fadeIn();
+            } else {
+              $(".header_i img").fadeOut();
+              $("#top_fixed").fadeOut();
+            }
+
+            if (index === 1) {
+              // 첫 번째 섹션에 도달했을 때 네비게이션 바 숨기기
+              $("#fp-nav").hide();
+
+              // 첫 번째 색션일 경우 상단 로고 색상 흰 색으로 
+              $('.menu_line').css({ 'background': '#fff' });
+              $('.logo_text').css({ 'color': '#fff' });
+
+              // 좌측 메뉴 노출
+              $('#lnb').css({ 'display': 'none' });
+            } else {
+              $("#fp-nav").show();
+
+              $('.menu_line').css({ 'background': '#000' });
+              $('.logo_text').css({ 'color': '#000' });
+
+              // 좌측 메뉴 노출
+              $('#lnb').css({ 'display': 'block' });
+            }
+
+            if (index === 2) {
+              $('#lnb .lnb_menu_01 ul li a').css({ 'color': '#fff' });
+            } else {
+              $('#lnb .lnb_menu_01 ul li a').css({ 'color': '#000' });
+            }
+
+            if(index === 4) {
+              $('#top-icon-img').attr('src', 'img/icon/i_top_button_white.png');
+            } else {
+              $('#top-icon-img').attr('src', 'img/icon/i_top_button.png');
+            }
+
+            // if (index === 3) {
+            //   $('#header').css({ 'border-bottom': '1px solid #000' });
+            // } else {
+            //   $('#header').css({ 'border-bottom': 'none' });
+            // }
+          },
+      });
+
+      // listSwiper
+        var section01Swiper = new Swiper(".section01_listSwiper", {
+          direction: 'vertical',
+          centeredSlides: true,
+          slidesPerView: 4,
+          loop: true,
+          autoplay: false, // 초기에는 autoplay 비활성화
+          on: {
+            activeIndexChange: function () {
+              const realIndex = this.realIndex;
+              $('.content-panel .right-wrap .description_wrap span:first-child').text(titles[realIndex]);
+              $('.content-panel .right-wrap .description_wrap span:last-child').text(descriptions[realIndex]);
+            }
+          }
+        });
+
+        $('#section_01_list_swiper_up').on('click', function() {
+          section01Swiper.slidePrev();
+        });
+
+        $('#section_01_list_swiper_down').on('click', function() {
+          section01Swiper.slideNext();
+        });
+
+        // 10초 후에 autoplay 시작
+        setTimeout(function() {
+          section01Swiper.params.autoplay = {
+            delay: 2000,
+            disableOnInteraction: false
+          };
+          section01Swiper.autoplay.start();
+        }, 10000); // 10000ms = 10초
+
         // listSwiper
         var listSwiper = new Swiper(".section01_listSwiper", {
           slidesPerView: 9,
@@ -232,6 +338,12 @@ $(document).ready(function () {
         });
     } else {
       const viewportHeight = $(window).height();
+      $(window).on('scroll', function () {
+        const scrollPosition = $(window).scrollTop();
+        $('.menu_line').css('background', scrollPosition >= viewportHeight ? '#000' : '#fff');
+        $('.logo_text').css('color', scrollPosition >= viewportHeight ? '#000' : '#fff');
+      });
+
         // 디바이스 크기가 992px 이하일 때
         var section01MobileSwiper = new Swiper(".section01_listMobileSwiper", {
           slidesPerView: 4,
@@ -365,10 +477,7 @@ $(document).ready(function () {
     }
   });
   
-
     const originalList = $('.marquee-item-list');
     const clonedList = originalList.clone();
     $('.marquee-block').append(clonedList);
-
-
 });
